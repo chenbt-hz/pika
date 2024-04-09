afcd802624780bb009d6fae9ed667f8b86305ec5
chenbt <34958405+chenbt-hz@users.noreply.github.com>
2024-04-09 10:35:26 +0800

    Update integrate_test.sh
    fix shell bug

--- tests/integration/integrate_test.sh
+++ tests/integration/integrate_test.sh
@@ -3,6 +3,7 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
+set -x
 go mod tidy
 
 export PATH="$PATH:$GOPATH/bin"
@@ -16,12 +17,12 @@ go get github.com/onsi/ginkgo/v2/ginkgo
 go install github.com/onsi/ginkgo/v2/ginkgo
 go get github.com/onsi/gomega/...
 
-echo "try ginkgo in PATH ----- ""
+echo "try ginkgo in PATH ----- "
 find /home -name ginkgo 2>/dev/null
 ginkgo --dry-run -v |grep -E -v "\[[0-9]+\.[0-9]+ seconds]"
 
 # try /home/runner/go/pkg/mod/github.com/bsm/ginkgo
-echo "try /home/runner/go/pkg/mod/github.com/bsm/ginkgo >> "
+echo "try /home/runner/go/pkg/mod/github.com/bsm/ginkgo ----- "
 /home/runner/go/pkg/mod/github.com/bsm/ginkgo --dry-run -v |grep -E -v "\[[0-9]+\.[0-9]+ seconds]"
 
 
