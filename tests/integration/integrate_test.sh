a9920b1f93e0d3b20caeba69f8fb6bc7b1f5db81
chenbt <34958405+chenbt-hz@users.noreply.github.com>
2024-04-09 09:19:48 +0800

    Update integrate_test.sh

--- tests/integration/integrate_test.sh
+++ tests/integration/integrate_test.sh
@@ -11,12 +11,23 @@ echo $GOPATH
 echo $GOBIN
 go env
 
-find / -name ginkgo 2>/dev/null
+# install ginkgo
+go get github.com/onsi/ginkgo/v2/ginkgo
+go install github.com/onsi/ginkgo/v2/ginkgo
+go get github.com/onsi/gomega/...
+
+echo "try ginkgo in PATH >> ""
+find /home -name ginkgo 2>/dev/null
 ginkgo --dry-run -v |grep -E -v "\[[0-9]+\.[0-9]+ seconds]"
 
-ls /home/runner/go/bin/
+# try /home/runner/go/pkg/mod/github.com/bsm/ginkgo
+echo "try /home/runner/go/pkg/mod/github.com/bsm/ginkgo >> "
+/home/runner/go/pkg/mod/github.com/bsm/ginkgo --dry-run -v |grep -E -v "\[[0-9]+\.[0-9]+ seconds]"
+
+
+ls -l /home/runner/go/bin/
 GinkgoPath="$(go env |grep GOPATH |awk -F "\"" '{ print $2}')/bin"/ginkgo
-echo $GinkgoPath
+echo "try $GinkgoPath >> "
 $GinkgoPath --dry-run -v |grep -E -v "\[[0-9]+\.[0-9]+ seconds]"
 
 go test -run=TestPikaWithCache -timeout 30m
